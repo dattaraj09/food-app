a304b0b043f1aa292e087f4d50ceb59a14fd81e6
Dattaraj <dattarajbirhade67@gmail.com>
2021-08-22 16:51:53 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+    return (
+        <>
+            <div className="nav_bar">
+                <div className="logo">
+                    <h2>Foodiffy</h2>
+                </div>
+                <div className="links">
+                    <ul>
+                        <li><a href="/">Eatclub</a></li>
+                        <li><a href="/">Deals</a></li>
+                        <li><a href="/">Cart</a></li>
+                        <li><a href="/">Singin</a></li>
+                    </ul>
+                </div>
+            </div>
+        
+        
+        
+        </>
+
+    )
+}
